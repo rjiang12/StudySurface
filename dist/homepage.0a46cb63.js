2559c62934ac5794ab07671961b35c7ec1cef820
rjiang12 <roy.jiang.4757@gmail.com>
2022-07-14 15:07:50 -0700

    parcel

---
+++ dist/homepage.0a46cb63.js
@@ -0,0 +1,139 @@
+//const { userEmail } = require('../index.js');
+//console.log(userEmail);
+function getName(info1) {
+    for (const el of info1){
+        console.log(el);
+        if (el.includes("firstname=")) {
+            const initial = el.indexOf("=") + 1;
+            const final = el.indexOf("%");
+            return el.slice(initial, final);
+        }
+    }
+    return "User";
+}
+function getEmail(info2) {
+    for (const el of info2){
+        if (el.slice(0, 6) === "email=") return el.slice(6);
+    }
+    return "";
+}
+const info = decodeURIComponent(document.cookie).split(";");
+const name = getName(info);
+const email = getEmail(info);
+const calendarLink = "https://calendar.google.com/calendar/embed?src=" + email + "&ctz=America%2FVancouver";
+let spotifyLink = "https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator&theme=0";
+if (document.querySelector(".container")) {
+    // add gCal iframe
+    document.querySelector(".rightBottom1").innerHTML += `
+        <iframe src=${calendarLink} frameborder="0" scrolling="no" id="calendar"></iframe>
+    `;
+    // add Spotify iframe
+    document.querySelector(".rightTop").innerHTML += `
+    <iframe src="${spotifyLink}" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" id="playlist"></iframe>
+    `;
+    // restore to-do list as necessary
+    if (localStorage.getItem("list-items")) document.querySelector(".to-do-list").innerHTML = localStorage.getItem("list-items");
+    // to-do list functionality 
+    document.addEventListener("keydown", (e)=>{
+        if (e.code === "Enter") {
+            document.querySelector(".to-do-list").innerHTML += `
+              <div class="task">
+                  <span id="taskname">
+                      <p>${document.querySelector("#my-input").value}</p>
+                  </span>
+                  <button class="delete">x</button>
+              </div>
+            `;
+            localStorage.setItem("list-items", document.querySelector(".to-do-list").innerHTML);
+            document.querySelector("#my-input").value = "";
+            const all_tasks2 = document.querySelectorAll(".delete");
+            for(let i2 = 0; i2 < all_tasks2.length; i2++)all_tasks2[i2].addEventListener("click", function() {
+                this.parentNode.remove();
+                localStorage.setItem("list-items", document.querySelector(".to-do-list").innerHTML);
+            });
+        }
+    });
+    // delete list items
+    const all_tasks1 = document.querySelectorAll(".delete");
+    for(let i1 = 0; i1 < all_tasks1.length; i1++)all_tasks1[i1].addEventListener("click", function() {
+        this.parentNode.remove();
+        localStorage.setItem("list-items", document.querySelector(".to-do-list").innerHTML);
+    });
+    // greet user by first name
+    document.querySelector(".userInfo").innerHTML += `
+    
+      <center>
+        <p>Welcome back, ${name} </p>
+      </center>
+      <center><button id="logout" onclick="logout()">Log out</button></center>
+    `;
+    // background change buttons
+    document.querySelector("#desk").addEventListener("click", ()=>{
+        document.body.style.background = 'rgba(0, 0, 0, 0.5) url("../assets/homepage_1.jpeg")';
+        document.body.style.backgroundSize = "cover";
+    });
+    document.querySelector("#forest").addEventListener("click", ()=>{
+        document.body.style.background = 'rgba(0, 0, 0, 0.5) url("../assets/forest.webp")';
+        document.body.style.backgroundSize = "cover";
+    });
+    document.querySelector("#city").addEventListener("click", ()=>{
+        document.body.style.background = 'rgba(0, 0, 0, 0.5) url("../assets/city.webp")';
+        document.body.style.backgroundSize = "cover";
+    });
+    document.querySelector("#mountain").addEventListener("click", ()=>{
+        document.body.style.background = 'rgba(0, 0, 0, 0.5) url("../assets/mountain.jpeg")';
+        document.body.style.backgroundSize = "cover";
+    });
+    document.querySelector("#stars").addEventListener("click", ()=>{
+        document.body.style.background = 'rgba(0, 0, 0, 0.5) url("../assets/stars.jpeg")';
+        document.body.style.backgroundSize = "cover";
+    });
+    document.querySelector("#blank").addEventListener("click", ()=>{
+        document.body.style.background = "gray";
+        document.body.style.backgroundSize = "cover";
+    });
+    // tool toggle buttons
+    document.querySelector("#toggleTimer").addEventListener("click", ()=>{
+        document.querySelector(".misc").hidden = !document.querySelector(".misc").hidden;
+        if (document.querySelector(".misc").hidden) document.querySelector(".misc").classList.add(".hide");
+        else document.querySelector(".misc").classList.remove(".hide");
+    });
+    document.querySelector("#toggleMusic").addEventListener("click", ()=>{
+        document.querySelector(".rightTop").hidden = !document.querySelector(".rightTop").hidden;
+        if (document.querySelector(".rightTop").hidden) document.querySelector(".rightTop").classList.add(".hide");
+        else document.querySelector(".rightTop").classList.remove(".hide");
+    });
+    document.querySelector("#toggleCalendar").addEventListener("click", ()=>{
+        document.querySelector(".rightBottom1").hidden = !document.querySelector(".rightBottom1").hidden;
+        if (document.querySelector(".rightBottom1").hidden) document.querySelector(".rightBottom1").classList.add(".hide");
+        else document.querySelector(".rightBottom1").classList.remove(".hide");
+    });
+    document.querySelector("#toggleList").addEventListener("click", ()=>{
+        document.querySelector(".rightBottom2").hidden = !document.querySelector(".rightBottom2").hidden;
+        if (document.querySelector(".rightBottom2").hidden) document.querySelector(".rightBottom2").classList.add(".hide");
+        else document.querySelector(".rightBottom2").classList.remove(".hide");
+    });
+    function getTime() {
+        let date = new Date();
+        let hh = date.getHours();
+        let mm = date.getMinutes();
+        let ss = date.getSeconds();
+        let period = "AM";
+        if (hh >= 12) period = "PM";
+        else period = "AM";
+        hh = hh < 10 ? "0" + hh : hh;
+        mm = mm < 10 ? "0" + mm : mm;
+        ss = ss < 10 ? "0" + ss : ss;
+        let time = hh + ":" + mm + ":" + ss + " " + period;
+        let inner = `<center id="tick"><center><div id="hours">${hh}</div></center>
+      <center><div id="minutes">${mm}</div></center>
+      <center><div id="period"> ${period}</div></center></center>`;
+        document.querySelector(".misc").innerHTML = inner;
+        setTimeout(function() {
+            getTime();
+        }, 1000);
+    }
+    getTime();
+}
+
+//# sourceMappingURL=homepage.0a46cb63.js.map
