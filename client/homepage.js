8c9355c7f4963c32e7055a89ce728e1b7a0cce92
rjiang12 <roy.jiang.4757@gmail.com>
2022-07-13 17:14:08 -0700

    togglers

--- client/homepage.js
+++ client/homepage.js
@@ -28,16 +28,21 @@ const calendarLink = "https://calendar.google.com/calendar/embed?src=" + email +
 let spotifyLink = "https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator&theme=0"
 
 if(document.querySelector('.container')) {
+
+    // add gCal iframe
     document.querySelector('.rightBottom1').innerHTML += `
         <iframe src=${calendarLink} frameborder="0" scrolling="no" id="calendar"></iframe>
     `; 
 
+    // add Spotify iframe
     document.querySelector('.rightTop').innerHTML += `
     <iframe src="${spotifyLink}" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" id="playlist"></iframe>
     `
 
+    // restore to-do list as necessary
     if(localStorage.getItem("list-items")) document.querySelector('.to-do-list').innerHTML = localStorage.getItem("list-items");
     
+      // to-do list functionality 
       document.addEventListener("keydown", (e) => {
         if(e.code === 'Enter') {
             document.querySelector('.to-do-list').innerHTML += `
@@ -61,6 +66,7 @@ if(document.querySelector('.container')) {
       }
     });
     
+    // delete list items
     const all_tasks = document.querySelectorAll(".delete");
     for(let i = 0; i < all_tasks.length; i++) {
         all_tasks[i].addEventListener("click", function(){
@@ -68,11 +74,87 @@ if(document.querySelector('.container')) {
         localStorage.setItem("list-items", document.querySelector('.to-do-list').innerHTML);
         });
     }
-    document.querySelector('.miscBottom').innerHTML += `
+
+
+    // greet user by first name
+    document.querySelector('.userInfo').innerHTML += `
       <center>
         <p>Welcome back, ${name} </p>
       </center>
       <center><button id="logout" onclick="logout()">Log out</button></center>
-    `
+    `;
+
+    // background change buttons
+    document.querySelector('#desk').addEventListener('click', () => {
+      document.body.style.background = 'rgba(0, 0, 0, 0.5) url("/assets/homepage_1.jpeg")';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    document.querySelector('#forest').addEventListener('click', () => {
+      document.body.style.background = 'rgba(0, 0, 0, 0.5) url("/assets/forest.webp")';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    document.querySelector('#city').addEventListener('click', () => {
+      document.body.style.background = 'rgba(0, 0, 0, 0.5) url("/assets/city.webp")';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    document.querySelector('#mountain').addEventListener('click', () => {
+      document.body.style.background = 'rgba(0, 0, 0, 0.5) url("/assets/mountain.jpeg")';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    document.querySelector('#stars').addEventListener('click', () => {
+      document.body.style.background = 'rgba(0, 0, 0, 0.5) url("/assets/stars.jpeg")';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    document.querySelector('#blank').addEventListener('click', () => {
+      document.body.style.background = 'gray';
+      document.body.style.backgroundSize = 'cover';
+    });
+
+    // tool toggle buttons
+    document.querySelector('#toggleTimer').addEventListener('click', () => {
+      document.querySelector('.misc').hidden = !document.querySelector('.misc').hidden;
+      if(document.querySelector('.misc').hidden) {
+        document.querySelector('.misc').classList.add('.hide'); 
+      }
+      else {
+        document.querySelector('.misc').classList.remove('.hide');
+      }
+    });
+
+    document.querySelector('#toggleMusic').addEventListener('click', () => {
+      document.querySelector('.rightTop').hidden = !document.querySelector('.rightTop').hidden;
+      if(document.querySelector('.rightTop').hidden) {
+        document.querySelector('.rightTop').classList.add('.hide'); 
+      }
+      else {
+        document.querySelector('.rightTop').classList.remove('.hide');
+      }
+    });
+
+    document.querySelector('#toggleCalendar').addEventListener('click', () => {
+      document.querySelector('.rightBottom1').hidden = !document.querySelector('.rightBottom1').hidden;
+      if(document.querySelector('.rightBottom1').hidden) {
+        document.querySelector('.rightBottom1').classList.add('.hide'); 
+      }
+      else {
+        document.querySelector('.rightBottom1').classList.remove('.hide');
+      }
+    });
+
+    document.querySelector('#toggleList').addEventListener('click', () => {
+      document.querySelector('.rightBottom2').hidden = !document.querySelector('.rightBottom2').hidden;
+      if(document.querySelector('.rightBottom2').hidden) {
+        document.querySelector('.rightBottom2').classList.add('.hide'); 
+      }
+      else {
+        document.querySelector('.rightBottom2').classList.remove('.hide');
+      }
+    });
+    
 }
     
